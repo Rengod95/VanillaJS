3029db5f0063aa288e083b2348c0aafaf9d307ae
Rengod95 <aorwn970@gmail.com>
2022-06-24 01:19:11 +0900

    코딩 테스트 준비

--- Grammer/implemention of caching with decorator.js
+++ Grammer/implemention of caching with decorator.js
@@ -1,15 +1,43 @@
-function myPow(num,Dnum){
-    return Math.pow(num,Dnum)
-}
+const HASH_NUM = 9973;
 
-function myPowDecorator(myPow,Hash){
-    let cache = new Map();
-    return function(num,Dnum){
-        let hashedKey = Hash(num,Dnum)
-        if(cache.has(hashedKey)){
-            return cache.get(hashedKey);
-        }
-        let notInCache = myPow.apply(this, aguments);
+const node = {
+  next: undefined,
+  value: undefined,
+  key: undefined,
+  id: undefined,
+
+  setValue(_value) {
+    this.value = _value;
+    return this.value;
+  },
+  setKey(_key) {
+    this.key = _key;
+    return this.key;
+  },
+  setId(_id) {
+    this.id = _id;
+    return this.id;
+  },
+  setNext(_node) {
+    this.next = _node;
+    return this.next;
+  },
+};
+
+const hashDecorator = (func) => {
+  let cache = new Map();
+  return function (x) {
+    if (cache.has(x)) {
+      console.log("해당 값이 이미 존재합니다.");
+      return;
     }
+    let result = func.call(this, x);
+    cache.set(x, result);
+    console.log(result);
+    return result;
+  };
+};
 
-}
+node.setValue = hashDecorator(node.setValue);
+node.setValue(1);
+node.setValue(1);
